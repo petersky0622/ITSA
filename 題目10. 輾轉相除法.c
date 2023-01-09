1a346038912a684991019f0f376243382cabcf07
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-01-09 15:44:52 +0800

    Create 題目10. 輾轉相除法.c

---
+++ 題目10. 輾轉相除法.c
@@ -0,0 +1,24 @@
+#include<stdio.h>      
+#include<string.h>      
+      
+      
+      
+int main(void){          
+        
+    int a, b, tmp;    
+    scanf("%d%d", &a, &b);    
+    if (a < b){    
+        tmp = a;    
+        a = b;    
+        b = tmp;    
+    }    
+    int max = 1;    
+    for (int i = 1; i <= b; i++){    
+        if (a % i == 0 && b % i == 0 && i > max){    
+            max = i;    
+        }    
+    }    
+    
+    printf("%d\n", max);    
+    return 0;          
+} 
