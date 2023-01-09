c260c56db9be006bc8185d42ca15ffbda4adc5c2
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-01-09 15:30:01 +0800

    Create [C_MM08-易] 計算兩數和的平方值.c

---
+++ [C_MM08-易] 計算兩數和的平方值.c
@@ -0,0 +1,13 @@
+#include<stdio.h>  
+#include<string.h>  
+  
+  
+  
+int main(void){      
+      
+    int a, b;  
+    scanf("%d%d", &a, &b);  
+    int c = a + b;  
+    printf("%d\n", c * c);  
+    return 0;      
+}
