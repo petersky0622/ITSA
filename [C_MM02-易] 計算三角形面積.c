40754dac86ab729c78474c51bd5d2fb6761eb634
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-01-09 15:24:00 +0800

    Create [C_MM02-易] 計算三角形面積.c

---
+++ [C_MM02-易] 計算三角形面積.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+#include<string.h>  
+  
+  
+  
+int main(void){      
+      
+    float a, b;  
+    scanf("%f%f", &a, &b);  
+    printf("%.1f\n", a * b / 2);  
+    return 0;      
+} 
