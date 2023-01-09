253ceb9aaa5fae088c19719b732059dcc8322e72
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-01-09 15:29:01 +0800

    Create [C_MM07-易] 計算平方值與立方值.c

---
+++ [C_MM07-易] 計算平方值與立方值.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+#include<string.h>  
+  
+  
+  
+int main(void){      
+      
+    int a;  
+    scanf("%d", &a );  
+    printf("%d %d %d\n",a, a * a, a * a * a);  
+    return 0;      
+}
