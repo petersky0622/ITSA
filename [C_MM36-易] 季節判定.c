d2772f91c3e9f97e05bfa6b0d360b60cdada7442
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-01-09 15:47:46 +0800

    Create [C_MM36-易] 季節判定.c

---
+++ [C_MM36-易] 季節判定.c
@@ -0,0 +1,26 @@
+#include<stdio.h>    
+    
+    
+int max(int a){    
+    
+}    
+    
+int main(void){    
+    
+    int a;    
+    scanf("%d", &a);    
+    if (a >= 3 && a <= 5){    
+        printf("Spring\n");    
+    }    
+    else if (a >= 6 && a <= 8){    
+        printf("Summer\n");    
+    }    
+    else if (a >= 9 && a <= 11){    
+        printf("Autumn\n");    
+    }    
+    else{    
+        printf("Winter\n");    
+    }    
+    
+    return 0;    
+}
