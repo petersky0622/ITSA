d84ba688704004579ab39034e84d08cbeb9a7fb8
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-01-09 16:07:12 +0800

    Create 題目9. 計算正整數被3整除之數值之總和.c

---
+++ 題目9. 計算正整數被3整除之數值之總和.c
@@ -0,0 +1,26 @@
+#include<stdio.h>  
+  
+int f(int n){  
+    if (n == 1 || n == 0){  
+        return n + 1;  
+    }  
+    else{  
+        return f(n - 1) + f(n / 2);  
+    }  
+}  
+  
+int main(void){  
+  
+    int a = 0;  
+    scanf("%d", &a);  
+    int ans = 0;  
+    for (int i = 1; i <= a; i++){  
+        if (i % 3 == 0){  
+            ans += i;  
+        }  
+    }  
+  
+    printf("%d\n", ans);  
+  
+    return 0;  
+}
