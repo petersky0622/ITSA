1424bc743a64df6d9c5ddd2e5305b60e92cf0f16
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-01-09 15:28:10 +0800

    Create [C_MM06-易] 英哩轉公里.c

---
+++ [C_MM06-易] 英哩轉公里.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+#include<string.h>  
+  
+  
+  
+int main(void){      
+      
+    double a = 0;  
+    scanf("%lf", &a);  
+    printf("%.1f\n", a * 1.6);  
+    return 0;      
+}
